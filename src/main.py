5a8fbbfe47e9b742a2c752e186071f4384537138
Jacob <jakesoto48@gmail.com>
2024-12-12 19:11:11 -0600

    re-structured file system

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from os import environ
+from dotenv import load_dotenv
+from bot import MusicBot
+
+
+
+# Entry Point
+def main() -> None:
+    load_dotenv()
+    myBot: MusicBot = MusicBot()
+    myBot.run(token=environ.get('DISCORD_BOT_KEY'))
+    
+
+if __name__ == '__main__':
+    main()
+
+
