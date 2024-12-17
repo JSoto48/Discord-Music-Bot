c326d334043b6573f0db521a74b597ecce47a80d
Jacob <jakesoto48@gmail.com>
2024-12-17 04:43:13 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,15 +3,13 @@ from dotenv import load_dotenv
 from bot import MusicBot
 
 
-
 # Entry Point
 def main() -> None:
     load_dotenv()
+    TOKEN = environ.get('DISCORD_BOT_KEY')
     myBot: MusicBot = MusicBot()
-    myBot.run(token=environ.get('DISCORD_BOT_KEY'))
+    myBot.run(token=TOKEN)
     
 
 if __name__ == '__main__':
     main()
-
-
