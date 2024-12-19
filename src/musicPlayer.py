e9c9999afa05702a468b55db8ad374a29354b46c
Jacob <jakesoto48@gmail.com>
2024-12-19 16:36:20 -0600

    removed join(), created embed

--- src/musicPlayer.py
+++ src/musicPlayer.py
@@ -1,15 +1,15 @@
 import yt_dlp as youtube_dl
 import discord
 from discord.ext import commands
-#from responses import Responses
-import asyncio
 
 
 class Song():
     def __init__(self, title: str = None, artist: str = None):
         self.title: str = title
         self.artist: str = artist
-        self.url: str = None
+        self.trackUrl: str = None
+        self.thumbnailURL: str = None
+
         self.ytdl_opts = {
             'format': 'bestaudio/best',         # Get the best audio quality
             'noplaylist': True,                 # Don't download playlists
@@ -18,13 +18,14 @@ class Song():
             'no_warnings': True,                 # Suppress warnings
         }
 
-    def getUrl(self) -> bool:
+
+    def getTrackUrl(self) -> bool:
         searchName = self.artist + ' ' + self.title + ' audio'
 
         with youtube_dl.YoutubeDL(self.ytdl_opts) as ytdl:
             trackInfo = ytdl.extract_info(searchName, download=False)
         if trackInfo is not None:
-            self.url = trackInfo['entries'][0]['url']
+            self.trackUrl = trackInfo['entries'][0]['url']
             return True
         else: return False
 
@@ -57,48 +58,70 @@ class QueueManager():
         self.FFmpegOptions = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 
-    def playNext(self, interaction: discord.Interaction = None):
+    def initSong(self, title: str, artist: str) -> Song:
+        newSong: Song = Song(title, artist)
+        newSong.getTrackUrl()
+        apiCall = self.spotifyAPI.search(q=title, limit=1, type='track')
+        results = apiCall['tracks']['items'][0]
+        images = results['album']['images']
+        newSong.thumbnailURL = images[len(images)-1]['url']
+        return newSong
+
+
+    def nowPlayingEmbed(self, track: Song) -> discord.Embed:
+        embed = discord.Embed(
+            title='Now Playing',
+            description=track.title,
+            colour=discord.Color.fuchsia()
+        )
+        embed.set_thumbnail(url=track.thumbnailURL)
+        return embed
+
+    async def playNext(self, interaction: discord.Interaction):
         if self.voiceClient is not None:
-            # Bot is in channel
+        # Ensure bot is in channel
             if self.queue.length > 0:
+            # Ensure there is songs in the queue
                 currentTrack = self.queue.pop()
-                self.voiceClient.play(source=discord.FFmpegPCMAudio(currentTrack.url, options=self.FFmpegOptions), after=lambda e: self.playNext())
-            #else:
-                # Play recommended songs
+                self.voiceClient.play(
+                    source=discord.FFmpegPCMAudio(currentTrack.trackUrl, options=self.FFmpegOptions),
+                    after= await self.playNext(interaction))
+
+                await interaction.channel.send(embed=self.nowPlayingEmbed(currentTrack))
+            # else:
+            #     #await interaction.response.send_message(f'Queue has ended')
+        else:
+            print('ERROR: Bot not in channel - from play')
 
 
-    async def join(self, interaction: discord.Interaction) -> bool:
-        userVoice = interaction.user.voice          # User voice channel activity
-        botVC = discord.utils.get(interaction.client.voice_clients, guild=interaction.guild)        # Bot voice client activity
+    async def addToQueue(self, songName: str, songArtist:str, interaction: discord.Interaction):
+        userVoice = interaction.user.voice
 
         if not userVoice:
-            return False
-        else:
-            if not botVC:
-                try:
-                    self.voiceClient = await userVoice.channel.connect()
-                    if self.voiceClient:
-                        return True
-                except Exception as e:
-                    return False
+        # User not in Voice channnel
+            await interaction.followup.send('Join a voice channel to play music')
+            return
 
+        newSong = self.initSong(title=songName, artist=songArtist)
 
-    async def addToQueue(self, track: Song, interaction: discord.Interaction) -> str:
         if self.voiceClient is not None:
-            # Bot is in channel and playing audio
+        # Bot is in voice channel
             if self.voiceClient.is_playing():
-                # Add to queue
-                self.queue.addSong(track=track, requestor=interaction.user.id)
-            else:
-                # Skip the paused song
-                self.queue.addSong(track=track, requestor=interaction.user.id)
+                self.queue.addSong(track=newSong, requestor=interaction.user.id)
+                await interaction.followup.send(f'Added {newSong.title} to the queue')
+            else:   # Bot is either paused or queue has ended
+                    # BUG ? Skips the paused song
+                self.queue.addSong(track=newSong, requestor=interaction.user.id)
                 self.playNext()
-            return (f'Added {track.title} to queue')
         else:
-            self.queue.addSong(track=track, requestor=interaction.user.id)
-            connected = await self.join(interaction=interaction)
-            if connected:
-                self.playNext()
-                return (f'Playing {track.title} by {track.artist}')
-            else:
-                return "Could not connect to voice"
+        # Bot not in voice channel
+            try:
+                self.voiceClient = await userVoice.channel.connect()
+            except Exception as e:
+                await interaction.followup.send(f'Error connecting to voice channel: {e.message}')
+                return
+            
+            self.queue.addSong(track=newSong, requestor=interaction.user.id)
+            await interaction.followup.send(f'Added {newSong.title} to the queue')
+            await self.playNext(interaction=interaction)
+
