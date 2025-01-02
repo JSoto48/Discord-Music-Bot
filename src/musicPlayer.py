61524f15f95d5c2ae9e0012f85878bff167be8ae
Jacob <jakesoto48@gmail.com>
2025-01-02 15:02:01 -0600

    changed SongQueue class to GuildInfo which now holds all music queues and voiceclient

--- src/musicPlayer.py
+++ src/musicPlayer.py
@@ -5,13 +5,19 @@ from asyncio import run_coroutine_threadsafe
 
 
 class Song():
-    def __init__(self, title: str = None, artist: str = None):
+    def __init__(self, title: str, artist: str, requestor: discord.User = None):
         self.title: str = title
         self.artist: str = artist
+        self.requestor: discord.User = requestor
+
         self.trackUrl: str = None
         self.trackID: str = None
         self.thumbnailURL: str = None
-        self.ytdl_opts = {
+
+
+    def getTrackUrl(self) -> bool:
+        query = f'{self.artist} {self.title}'
+        ytdl_opts = {
             'format': 'bestaudio/best',         # Get the best audio quality
             'noplaylist': True,                 # Don't download playlists
             'quiet': True,                      # Show progress
@@ -19,11 +25,7 @@ class Song():
             'no_warnings': True,                # Suppress warnings
         }
 
-
-    def getTrackUrl(self) -> bool:
-        query = f'{self.artist} {self.title}'
-
-        with youtube_dl.YoutubeDL(self.ytdl_opts) as ytdl:
+        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
             trackInfo = ytdl.extract_info(query, download=False)
         if trackInfo is not None:
             self.trackUrl = trackInfo['entries'][0]['url']
@@ -32,138 +34,146 @@ class Song():
 
 
 
-class SongQueue():
-    def __init__(self):
-        self.__queue: list[Song] = []         # FIFO queue
-        self.length = 0
+class GuildInfo():
+    def __init__(self, guildID:int, voiceClient: discord.VoiceClient = None):
+        self.guildID:int = guildID
+        self.voiceClient: discord.VoiceClient = voiceClient
+        self.nowPlayingMessage: discord.Message = None
 
-    def addSong(self, track: Song, requestor: str = None) -> None:
-        # Adds a song to the queue
-        #newElement = (track, requestor)
-        self.length += 1
-        self.__queue.append(track)
-
-    def pop(self) -> Song:
-        # Removes and returns song from the first index of the queue
-        self.length -= 1
-        return self.__queue.pop(0)
-
-    def peek(self) -> Song:
-        # Returns first song without removing it
-        firstSong = self.__queue.pop(0)
-        self.__queue.insert(0, firstSong)
-        return firstSong
+        self.__history: list[Song] = []     # LIFO queue
+        self.__songQueue: list[Song] = []
+        self.__recQueue: list[Song] = []
     
-    def clear(self) -> None:
-        self.length = 0
-        self.__queue.clear()
 
-
-
-class QueueManager():
-    def __init__(self, spotifyAPI, lastFM):
-        self.musicQueues: (int, [SongQueue, SongQueue]) = {}  # KEY: guildID | VALUE: list[0]=song queue, list[1]=recommended song queue
-        self.voiceClients: (int, discord.VoiceClient) = {}
-        #self.queueControls = None
-        self.nowPlayingMessage = None
-        self.lastSong: Song = None
-
-        self.spotifyAPI = spotifyAPI
-        self.lastFM = lastFM
-        self.FFmpegOptions = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
-            'options': '-vn'}
-
-
-    async def addToQueue(self, songName: str, songArtist:str, interaction: discord.Interaction):
-        interactionGuild: int = interaction.guild_id                # Guild = discord server
-        userVoice: discord.VoiceState = interaction.user.voice
-
-        if not userVoice:
-            await interaction.followup.send('Join a voice channel to play music')
-            return
-        newSong: Song = Song(title=songName, artist=songArtist)
-        matchingClient: discord.VoiceClient = self.voiceClients.get(interactionGuild)
-
-        if matchingClient is None or not matchingClient.is_connected():
-            # Bot is not in voice channel
-            # self.clear(interaction)
-            newSong.getTrackUrl()
-            try:
-                newVoiceClient: discord.VoiceClient = await userVoice.channel.connect()
-            except Exception as e:
-                await interaction.followup.send(f'Failed to connect to voice: {e}')
-                
-            await interaction.followup.send(f'Added {newSong.title} by {newSong.artist} to the queue')
-            await self.play(newSong, interaction, newVoiceClient)
-        else:
-            # Bot is in voice channel
-            self.musicQueues.get(interactionGuild)[0].addSong(newSong, requestor=interaction.user.name)
-            if matchingClient.is_playing() or matchingClient.is_paused():
-                await interaction.followup.send(f'Added {newSong.title} by {newSong.artist} to the queue')
-            else:
-                await interaction.followup.send(f'Added {newSong.title} by {newSong.artist} to the queue')
-                self.playNext(interaction)
-
-
-    def playNext(self, interaction) -> discord.Embed:
-        guildID = interaction.guild_id
-        songQueue: SongQueue = self.musicQueues.get(guildID)[0]
-        currentTrack: Song = None
-
-        if songQueue.length > 0:
-            # Songs added by users
-            currentTrack = songQueue.pop()
-            self.lastSong = currentTrack
-        else:
-            recQueue: SongQueue = self.musicQueues.get(guildID)[1]
-            if self.lastSong is not None:
-                # Get recommended songs based on the last song queued by users
-                self.getSimilar(self.lastSong, guildID=guildID)
-                self.lastSong = None
-                currentTrack = recQueue.pop()
-            elif recQueue.length > 0:
-                # Keep playing recommeneded songs added by bot
-                currentTrack = recQueue.pop()
-
-        currentVC: discord.VoiceClient = self.voiceClients.get(guildID)
-        if currentVC.is_playing() or currentVC.is_paused():
-            currentVC.stop()
-        
-        currentTrack = self.getSongInfo(currentTrack)
-        currentVC.play(discord.FFmpegPCMAudio(currentTrack.trackUrl, options=self.FFmpegOptions), after=lambda e: self.playNext(interaction))
-
-        trackEmbed: discord.Embed = self.getNowPlayingEmbed(currentTrack)
+    def clearQueues(self) -> None:
+        if len(self.__songQueue) > 0:
+            self.__songQueue.clear()
+        if len(self.__recQueue) > 0:
+            self.__recQueue.clear()
+        if len(self.__history) > 0:
+            self.__history.clear()
         if self.nowPlayingMessage is not None:
-            run_coroutine_threadsafe(self.nowPlayingMessage.edit(embed=trackEmbed), interaction.client.loop)
-        else:
-            return trackEmbed
-
-    
-    async def play(self, track: Song, interaction: discord.Interaction, newVoiceClient: discord.VoiceClient):
-        guildID = interaction.guild_id
-        self.voiceClients[guildID] = newVoiceClient
-        self.musicQueues[guildID] = [SongQueue(), SongQueue()]
-        self.musicQueues.get(guildID)[0].addSong(track)
-        self.nowPlayingMessage = await interaction.channel.send(
-            embed=self.playNext(interaction),
-            view=QueueControls(queueManager=self, voiceClient=self.voiceClients.get(guildID)))
-
-
-    def clear(self, interaction):
-        guildID = interaction.guild_id
-        musicQueues = self.musicQueues.get(guildID)
-        for queue in musicQueues:
-            if queue.length > 0:
-                queue.clear()
-    
-        if self.nowPlayingMessage is not None:
-            self.nowPlayingMessage.clear()
+            self.nowPlayingMessage.delete()
             self.nowPlayingMessage = None
 
 
+    def queueSong(self, track: Song) -> None:
+        if track.requestor is None:
+            self.__recQueue.append(track)
+        else:
+            self.__songQueue.append(track)
+
+    
+    def getSong(self) -> Song:
+        poppedSong: Song = None
+        if len(self.__songQueue) > 0:
+            poppedSong = self.__songQueue.pop(0)
+            self.__recQueue.clear()
+            self.__history.insert(0, poppedSong)    # Check history length to conserve memory
+        elif len(self.__recQueue) > 0:
+            poppedSong = self.__recQueue.pop(0)
+            self.__history.insert(0, poppedSong)    # Check history length to conserve memory
+        return poppedSong
+    
+    def peekPrevious(self) -> Song:
+        if len(self.__history) > 0:
+            return self.__history[0]
+    
+    def popPrevious(self) -> Song:
+        if len(self.__history) > 0:
+            return self.__history.pop(0)
+
+
+
+class QueueManager(discord.ui.View):
+    def __init__(self, spotifyAPI, lastFM):
+        #super().__init__(timeout=None)
+        self.spotifyAPI = spotifyAPI
+        self.lastFM = lastFM
+        self.guilds: (int, GuildInfo) = {}
+
+    
+    @discord.ui.button(emoji='⏯', style=discord.ButtonStyle.grey)
+    async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        vc = self.guilds.get(interaction.guild_id).voiceClient
+        if vc.is_paused():
+            vc.resume()
+        elif vc.is_playing():
+            vc.pause()
+
+
+    @discord.ui.button(emoji='⏭', style=discord.ButtonStyle.grey)
+    async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.playNext(interaction)
+
+
+    async def addToQueue(self, songName: str, songArtist:str, interaction: discord.Interaction):
+        if not interaction.user.voice:
+            await interaction.followup.send('Join a voice channel to play music')
+            return
+
+        guildInfo = self.guilds.get(interaction.guild_id)
+        if guildInfo is None:
+            try:
+                newVoiceClient: discord.VoiceClient = await interaction.user.voice.channel.connect()
+            except Exception as e:
+                await interaction.followup.send(f'Failed to connect to voice: {e}')
+                return
+            self.guilds.update({interaction.guild_id: GuildInfo(guildID=interaction.guild_id, voiceClient=newVoiceClient)})
+            guildInfo = self.guilds.get(interaction.guild_id)
+        elif guildInfo.voiceClient is None or not guildInfo.voiceClient.is_connected():
+            guildInfo.clearQueues()
+            try:
+                newVoiceClient: discord.VoiceClient = await interaction.user.voice.channel.connect()
+            except Exception as e:
+                await interaction.followup.send(f'Failed to connect to voice: {e}')
+                return
+            guildInfo.voiceClient = newVoiceClient
+
+        newSong: Song = Song(title=songName, artist=songArtist, requestor=interaction.user)
+        guildInfo.queueSong(newSong)
+        await interaction.followup.send(f'Added {newSong.title} by {newSong.artist} to the queue')
+        if not guildInfo.voiceClient.is_playing() and not guildInfo.voiceClient.is_paused():
+           self.playNext(interaction)
+
+
+    def playNext(self, interaction):
+        guildInfo = self.guilds.get(interaction.guild_id)
+        
+        nextSong: Song = guildInfo.getSong()
+        if nextSong is None:
+            prevSong = guildInfo.peekPrevious()
+            if prevSong is not None:
+                self.getSimilar(prevSong, guildInfo)
+            else:
+                print("ERROR: No previous song to base rec's on")
+                return
+            nextSong = guildInfo.getSong()
+
+        if guildInfo.voiceClient.is_playing() or guildInfo.voiceClient.is_paused():
+            guildInfo.voiceClient.stop()
+        
+        nextSong = self.getSongInfo(nextSong)
+        FFmpegOptions = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
+            'options': '-vn'}
+        guildInfo.voiceClient.play(discord.FFmpegPCMAudio(nextSong.trackUrl, options=FFmpegOptions), after=lambda e: self.playNext(interaction))
+
+        trackEmbed: discord.Embed = self.getNowPlayingEmbed(nextSong)
+        if guildInfo.nowPlayingMessage is None:
+            run_coroutine_threadsafe(self.initQueueControls(interaction, trackEmbed), interaction.client.loop)
+        else:
+            run_coroutine_threadsafe(guildInfo.nowPlayingMessage.edit(embed=trackEmbed), interaction.client.loop)
+
+    
+    async def initQueueControls(self, interaction: discord.Interaction, embed: discord.Embed) -> None:
+        guildInfo = self.guilds.get(interaction.guild_id)
+        guildInfo.nowPlayingMessage = await interaction.channel.send(embed=embed, view=self)
+
+
     def getSongInfo(self, track: Song) -> Song:
-        track.getTrackUrl()
         query = track.artist+' '+track.title
         apiCall = self.spotifyAPI.search(q=query, limit=1, type='track')
         results = apiCall['tracks']['items'][0]
@@ -171,20 +181,16 @@ class QueueManager():
         track.trackID = results['uri']
         images = results['album']['images']
         track.thumbnailURL = images[len(images)-1]['url']
+        track.getTrackUrl()
         return track
 
 
-    def getSimilar(self, track: Song, guildID: int):
-        # Based on listening data, adds similar tracks to the guildID's recommended queue
-        recQueue = self.musicQueues.get(guildID)[1]
-        if recQueue.length > 0:
-            recQueue.clear()
-        
+    def getSimilar(self, track: Song, guildInfo: GuildInfo):     
         trackObj = self.lastFM.get_track(artist=track.artist, title=track.title)
         response = trackObj.get_similar(limit=20)
         for similarItem in response:
             recSong = Song(title=str(similarItem.item.title), artist=str(similarItem.item.artist))
-            recQueue.addSong(track=recSong)
+            guildInfo.queueSong(recSong)
 
 
     def getNowPlayingEmbed(self, track: Song) -> discord.Embed:
@@ -196,33 +202,4 @@ class QueueManager():
         embed.set_thumbnail(url=track.thumbnailURL)
         return embed
 
-    def getPausedEmbed(self) -> discord.Embed:
-        embed = discord.Embed(
-            title='Pasued',
-            colour=discord.Color.fuchsia()
-        )
-        return embed
-
-
-class QueueControls(discord.ui.View):
-    def __init__(self, queueManager: QueueManager, voiceClient: discord.VoiceClient):
-        super().__init__(timeout=None)
-        self.queueManager = queueManager
-        self.voiceClient = voiceClient
-
-
-    @discord.ui.button(emoji='⏯', style=discord.ButtonStyle.grey)
-    async def pause(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
-        if self.voiceClient.is_paused():
-            self.voiceClient.resume()
-        elif self.voiceClient.is_playing():
-            self.voiceClient.pause()
-            await interaction.message.edit(embed=self.queueManager.getPausedEmbed())
-
-
-    @discord.ui.button(emoji='⏭', style=discord.ButtonStyle.grey)
-    async def skip(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
-        trackEmbed = self.queueManager.playNext(interaction)
 
